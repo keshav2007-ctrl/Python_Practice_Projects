74c9ee6b3eebe5a93822f6aaa30458d8898064f2
Keshav Bhardwaj <bhardwajkeshav2007@gmail.com>
2026-06-30 13:36:46 +0530

    new problem solved

--- python/Practice_Codes/Inheritence.py
+++ python/Practice_Codes/Inheritence.py
@@ -87,3 +87,21 @@ o = Manager()
 print(o.a, o.b, o.c) #if i only run this it will also run programmer class's constructor because of super().__init__()
 
 
+class Employee:
+    salary = 234
+    increment = 20
+
+    @property
+    def SalaryAfterIncrement(self):
+        return self.salary + (self.salary*self.increment/100)
+
+    @SalaryAfterIncrement.setter
+    def SalaryAfterIncrement(self, salary):
+
+        self.increment = ((salary/self.salary)-1)*100
+    
+
+e = Employee()
+e.SalaryAfterIncrement = 280.9
+print(e.increment)
+
